505f8c1e13739f7a89548229db5970b6526cd386
Oliver Lomax <oliver.lomax@abingdon.org.uk>
2020-02-12 19:25:36 +0000

    working neural net

--- neural_net.py
+++ neural_net.py
@@ -1,13 +1,35 @@
 import numpy
-import random
+#import random
+
+def sigmoid(z):
+    return 1.0 / (1.0 + numpy.exp(-z))
 
 class Network:
     def __init__(self, layer_sizes):
         self.layer_sizes = layer_sizes
         self.layers = len(layer_sizes)
         self.biases = [numpy.random.randn(y, 1) for y in layer_sizes[1:]]
+        self.weights = [numpy.random.randn(y, x) for x, y in zip(layer_sizes[:-1], layer_sizes[1:])]
+
+    def feedforward(self, a):
+        """ a: a numpy matrix (n ,1) representing the input """
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(numpy.dot(w, a) + b)
+        return a
 
 
-net = Network([3,5,2])
-print(net.biases)
+net = Network([2,5,1])
+inp00 = numpy.array([[0],[0]])
+inp10 = numpy.array([[1],[0]])
+inp01 = numpy.array([[0],[1]])
+inp11 = numpy.array([[1],[1]])
+print(net.feedforward(inp00))
+print(net.feedforward(inp01))
+print(net.feedforward(inp10))
+print(net.feedforward(inp11))
+# for i in range(net.layers-1):
+#     print(net.biases[i], net.weights[i])
+#     print("---")
+# print(net.weights[0])
 
+# print(sigmoid(net.weights[0]))
